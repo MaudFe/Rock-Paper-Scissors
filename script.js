1fb26361fe5b7bca60e6a30e42d63b0c6b39685e
Maud Ferrer <maud.ferrer9@orange.fr>
2022-01-06 16:42:05 +0100

    Complete JS file to make game playable from borwser console

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log('Hello, World!');
+console.log("Hello, World!");
+
+let userScore = 0;
+let computerScore = 0;
+let numberRounds = 1;
+
+function computerPlay() {
+  let choices = ["rock", "paper", "scissors"];
+  let computerChoice = Math.floor(Math.random() * choices.length);
+
+  return choices[computerChoice];
+}
+
+function playRound(playerSelection, computerSelection) {
+  const insensitivePlayerSel = playerSelection.toLowerCase();
+  numberRounds++;
+  if (
+    (insensitivePlayerSel === "rock" && computerSelection === "scissors") ||
+    (insensitivePlayerSel === "scissors" && computerSelection === "paper") ||
+    (insensitivePlayerSel === "paper" && computerSelection === "rock")
+  ) {
+    userScore++;
+    return `You win! ${insensitivePlayerSel} beats ${computerSelection}.`;
+  } else if (insensitivePlayerSel === computerSelection) {
+    return `Tie! ${insensitivePlayerSel} <=> ${computerSelection}.`;
+  } else {
+    computerScore++;
+    return `You lose! ${computerSelection} beats ${insensitivePlayerSel}.`;
+  }
+}
+
+function playGame() {
+
+  while (userScore < 5 && computerScore < 5) {
+    let playerSelection = prompt("Rock, Paper, or Scissors?");
+    let computerSelection = computerPlay();
+    console.log(`Round number ${numberRounds}:`);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`Score: User ${userScore} - ${computerScore} Computer`)
+  }
+
+  if (userScore === 5) {
+    return `Congrats! You win the game in ${numberRounds - 1} rounds!`;
+  } else if (computerScore === 5) {
+    return `Too bad! You lose in ${numberRounds - 1} rounds!`;
+  }
+}
+
+console.log(playGame());
