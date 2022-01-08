108a202029008820636edb9dc66c11af0ead80d3
Maud Ferrer <maud.ferrer9@orange.fr>
2022-01-08 18:47:30 +0100

    Modify main code to add interface, no css

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-console.log("Hello, World!");
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
 
 let userScore = 0;
 let computerScore = 0;
+let tieRounds = 0;
 let numberRounds = 1;
 
 function computerPlay() {
@@ -12,38 +15,75 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  const insensitivePlayerSel = playerSelection.toLowerCase();
   numberRounds++;
   if (
-    (insensitivePlayerSel === "rock" && computerSelection === "scissors") ||
-    (insensitivePlayerSel === "scissors" && computerSelection === "paper") ||
-    (insensitivePlayerSel === "paper" && computerSelection === "rock")
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    userScore++;
-    return `You win! ${insensitivePlayerSel} beats ${computerSelection}.`;
-  } else if (insensitivePlayerSel === computerSelection) {
-    return `Tie! ${insensitivePlayerSel} <=> ${computerSelection}.`;
+    ++userScore;
+    updateScore();
+    if (playGame()) {
+      userScore = computerScore = tieRounds = 0;
+      updateScore();
+    }
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else if (playerSelection === computerSelection) {
+    ++tieRounds;
+    updateScore();
+    if (playGame()) {
+      userScore = computerScore = tieRounds = 0;
+      updateScore();
+    }
+    return `Tie! ${playerSelection} <=> ${computerSelection}.`;
   } else {
-    computerScore++;
-    return `You lose! ${computerSelection} beats ${insensitivePlayerSel}.`;
+    ++computerScore;
+    updateScore();
+    if (playGame()) {
+      userScore = computerScore = tieRounds = 0;
+      updateScore();
+    }
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
   }
+}
+
+function clickListener(e) {
+  const playerSelection = e.target.id;
+  const computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+}
+
+rockBtn.addEventListener('click', clickListener);
+paperBtn.addEventListener('click', clickListener);
+scissorsBtn.addEventListener('click', clickListener);
+
+function updateScore() {
+  document.getElementById('player-score').textContent = userScore;
+  document.getElementById("computer-score").textContent = computerScore;
+  document.getElementById("tie-score").textContent = tieRounds;
 }
 
 function playGame() {
-
-  while (userScore < 5 && computerScore < 5) {
-    let playerSelection = prompt("Rock, Paper, or Scissors?");
-    let computerSelection = computerPlay();
-    console.log(`Round number ${numberRounds}:`);
-    console.log(playRound(playerSelection, computerSelection));
-    console.log(`Score: User ${userScore} - ${computerScore} Computer`)
-  }
+  const results = document.querySelector("#results");
+  const div = document.createElement('div');
+  div.setAttribute('id', 'res');
 
   if (userScore === 5) {
-    return `Congrats! You win the game in ${numberRounds - 1} rounds!`;
+    div.textContent = `Congrats! You win the game in ${numberRounds - 1} rounds!`;
+    results.appendChild(div);
+    return true;
   } else if (computerScore === 5) {
-    return `Too bad! You lose in ${numberRounds - 1} rounds!`;
+    div.textContent = `Too bad! You lose in ${numberRounds - 1} rounds!`;
+    results.appendChild(div);
+    return true;
   }
+  if (document.body.contains(document.getElementById("res"))) {
+    document.getElementById("res").remove();
+  }
+  return false;
 }
 
-console.log(playGame());
+if (playGame()) {
+  userScore = computerScore = tieRounds = 0;
+  updateScore();
+}
