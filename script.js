e1593482f79fbcffd263472fbfc74d260e06a70f
Maud Ferrer <maud.ferrer9@orange.fr>
2022-01-18 18:12:42 +0100

    Simplify logic of the game, add button to start new game (refresh page)

--- script.js
+++ script.js
@@ -1,90 +1,68 @@
-const rockBtn = document.querySelector('#rock');
-const paperBtn = document.querySelector('#paper');
-const scissorsBtn = document.querySelector('#scissors');
-
-let userScore = 0;
+let playerScore = 0;
 let computerScore = 0;
-let tieRounds = 0;
-let numberRounds = 1;
+let rounds = 0;
+const pScore = document.querySelector(".p-score");
+const cScore = document.querySelector(".c-score");
+const result = document.querySelector(".result");
+const options = document.querySelectorAll(".button");
+let playerChoice;
+let computerChoice;
+
 
 function computerPlay() {
-  let choices = ["rock", "paper", "scissors"];
-  let computerChoice = Math.floor(Math.random() * choices.length);
+  const choices = ["rock", "paper", "scissors"];
+  const computerPlay = Math.floor(Math.random() * choices.length);
 
-  return choices[computerChoice];
+  return choices[computerPlay];
 }
 
+
+options.forEach(option => option.addEventListener('click', (e) => {
+  playerChoice = e.target.id;
+  computerChoice = computerPlay();
+  playRound(playerChoice, computerChoice);
+} ))
+
+
 function playRound(playerSelection, computerSelection) {
-  numberRounds++;
+  rounds++;
   if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    ++userScore;
-    updateScore();
-    if (playGame()) {
-      userScore = computerScore = tieRounds = 0;
-      updateScore();
-    }
-    return `You win! ${playerSelection} beats ${computerSelection}.`;
+    playerScore++;
+    result.innerText = `You win! ${playerSelection} beats ${computerSelection}.`;
   } else if (playerSelection === computerSelection) {
-    ++tieRounds;
-    updateScore();
-    if (playGame()) {
-      userScore = computerScore = tieRounds = 0;
-      updateScore();
-    }
-    return `Tie! ${playerSelection} <=> ${computerSelection}.`;
+    result.innerText = `Draw! ${playerSelection} === ${computerSelection}.`;
   } else {
-    ++computerScore;
-    updateScore();
-    if (playGame()) {
-      userScore = computerScore = tieRounds = 0;
-      updateScore();
-    }
-    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    computerScore++;
+    result.innerText = `You lose! ${computerSelection} beats ${playerSelection}.`;
   }
+  updateScore();
+  playGame();
 }
 
-function clickListener(e) {
-  const playerSelection = e.target.id;
-  const computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-}
-
-rockBtn.addEventListener('click', clickListener);
-paperBtn.addEventListener('click', clickListener);
-scissorsBtn.addEventListener('click', clickListener);
 
 function updateScore() {
-  document.getElementById('player-score').textContent = userScore;
-  document.getElementById("computer-score").textContent = computerScore;
-  document.getElementById("tie-score").textContent = tieRounds;
+  pScore.innerText = playerScore;
+  cScore.innerText = computerScore;
 }
+
 
 function playGame() {
-  const results = document.querySelector("#results");
-  const div = document.createElement('div');
-  div.setAttribute('id', 'res');
 
-  if (userScore === 5) {
-    div.textContent = `Congrats! You win the game in ${numberRounds - 1} rounds!`;
-    results.appendChild(div);
-    return true;
+  const optionsDisplay = document.querySelector(".options");
+  if (playerScore === 5) {
+    result.innerText = `Congrats! You win the game in ${rounds} rounds!`;
+    optionsDisplay.style.display = "none";
   } else if (computerScore === 5) {
-    div.textContent = `Too bad! You lose in ${numberRounds - 1} rounds!`;
-    results.appendChild(div);
-    return true;
+    result.innerText = `Too bad! You lose in ${rounds} rounds!`;
+    optionsDisplay.style.display = "none";
   }
-  if (document.body.contains(document.getElementById("res"))) {
-    document.getElementById("res").remove();
-    numberRounds = 0;
-  }
-  return false;
 }
 
-if (playGame()) {
-  userScore = computerScore = tieRounds = 0;
-  updateScore();
-}
+document.querySelector(".new-game").addEventListener('click', () => {
+  location.reload();
+})
+
